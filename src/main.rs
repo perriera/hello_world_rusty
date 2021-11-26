8a56002ede5031cdf42c22c6a102aa86b7836256
Perry Anderson <perry.anderson@gmail.com>
2021-11-26 14:41:17 -0800

    Changed tasks.json

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@
 //
 
 fn main() {
-   let array = ["This","is","an","array"];
-   let array_slice = &array[1..4];
-   
+   let mut vector1: Vec<i32> = Vec::new();
+   let mut vector2: Vec<i32> = vec![1,2,3,4,5];
+   let mut vector1: Vec<i32> = Vec::with_capacity(10);
+
+   let values = vec![1,2,3];
+   for a in values {
+       println!("{}", a);
+   }
 
 }
 
