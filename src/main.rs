74c3a6e8406b44f173e33026efaf297e6b0eb1a3
Perry Anderson <perry.anderson@gmail.com>
2021-11-26 15:57:14 -0800

    Added owner

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,11 @@
 //
 
 fn main() {
-   let mut vector1: Vec<i32> = Vec::new();
-   let mut vector2: Vec<i32> = vec![1,2,3,4,5];
-   let mut vector1: Vec<i32> = Vec::with_capacity(10);
-
-   let values = vec![1,2,3];
-   for a in values {
-       println!("{}", a);
-   }
-
-   let array = ["This","is","an","array"];
-   let array_slice = &array[0..4];
+    let x = 1;
+    {
+        let y = 2;
+        println!("x: {}\n y: {}",x,y);
+    }
 
 }
 
