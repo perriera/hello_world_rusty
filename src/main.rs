4e6e0f87236635bbf02b795228e32bdb69e6cdcb
Perry Anderson <perry.anderson@gmail.com>
2021-11-25 18:01:00 -0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@
 fn main() {
     let somevalue = 123;
     println!("Hello, world!!! {}", somevalue);
+    let x = 5;
+    let y = 8;
+    if x > y {
+        println!("{} is smaller than {}", x, y);
+    } else {
+        println!("{} is greater than {}", x, y);
+    }
 }
 
-// 
+//
