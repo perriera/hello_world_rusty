d02a28ce1f8bfbafe5919e8e60e74c92e58bdbab
Perry Anderson <perry.anderson@gmail.com>
2021-11-27 15:54:12 -0800

    Used a data type

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,37 @@
 //
 
 #[derive(Debug)]
+enum UserType {
+    Guest,
+    Regular,
+    Admin,
+}
+
+#[derive(Debug)]
 struct User {
     name: String,
     email: String,
     age: u16,
+    user_type: UserType,
 }
 
 impl User {
     pub fn print_user(self) {
         println!(
-            "The name of the user is {}.\nHis email is: {}.\nHe is {} old",
-            self.name, self.email, self.age
+            "The name of the user is {}.\nHis email is: {}.\nHe is {} old\nType of user: {:?}",
+            self.name, self.email, self.age, self.user_type
         )
     }
 }
+
 fn main() {
+    let user_type = UserType::Regular;
+    println!("{:?}", user_type);
     let new_user = User {
         name: "Dave".to_string(),
         email: "david@mail.com".to_string(),
         age: 32,
+        user_type: UserType::Regular,
     };
 
     // println!("{:#?}", new_user);
