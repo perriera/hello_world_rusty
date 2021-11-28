dbbcd15fbc2199b3158d5d9c602505e8467d38e5
Perry Anderson <perry.anderson@gmail.com>
2021-11-27 16:04:57 -0800

    Added a crate

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,16 @@
 // https://levelup.gitconnected.com/rust-with-visual-studio-code-46404befed8
 //
 
-#[derive(Debug)]
-enum UserType {
-    Guest,
-    Regular,
-    Admin,
-}
-
-#[derive(Debug)]
-struct User {
-    name: String,
-    email: String,
-    age: u16,
-    user_type: UserType,
-}
-
-impl User {
-    pub fn print_user(self) {
-        println!(
-            "The name of the user is {}.\nHis email is: {}.\nHe is {} old\nType of user: {:?}",
-            self.name, self.email, self.age, self.user_type
-        )
-    }
-}
+extern crate some_crate;
 
 fn main() {
-    let user_type = UserType::Regular;
+    let user_type = some_crate::UserType::Regular;
     println!("{:?}", user_type);
-    let new_user = User {
+    let new_user = some_crate::User {
         name: "Dave".to_string(),
         email: "david@mail.com".to_string(),
         age: 32,
-        user_type: UserType::Regular,
+        user_type: some_crate::UserType::Regular,
     };
 
     // println!("{:#?}", new_user);
---
+++ src/some_crate/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub enum UserType {
+    Guest,
+    Regular,
+    Admin,
+}
+
+#[derive(Debug)]
+pub struct User {
+    pub name: String,
+    pub email: String,
+    pub age: u16,
+    pub user_type: UserType,
+}
+
+impl User {
+    pub fn print_user(self) {
+        println!(
+            "The name of the user is {}.\nHis email is: {}.\nHe is {} old\nType of user: {:?}",
+            self.name, self.email, self.age, self.user_type
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
