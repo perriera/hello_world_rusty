cf82707d62b675ee0fad32caf8bc979b16c6a11c
Perry Anderson <perry.anderson@gmail.com>
2021-11-28 15:16:48 -0800

    Added threads

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,14 @@
 // https://github.com/reem/stainless
 //
 
-#[derive(Debug)]
-struct Hero {
-    name: String,
-    energy: u16,
-    strike: bool,
-}
-#[derive(Debug)]
-struct Goblin {
-    energy: u16,
-    strike: bool,
-}
-
-impl Hero {
-    fn jump(&self) {}
-}
-
-trait StrikeTrait {
-    fn strike(&mut self);
-}
-
-impl StrikeTrait for Hero {
-    fn strike(&mut self) {
-        self.strike = true;
-    }
-}
-
-impl StrikeTrait for Goblin {
-    fn strike(&mut self) {
-        self.strike = true;
-    }
-}
+use std::thread;
 
 fn main() {
-    let mut hero = Hero {
-        name: "Dave".to_string(),
-        energy: 100,
-        strike: false,
-    };
-    let mut goblin = Goblin {
-        energy: 99,
-        strike: false,
-    };
-    println!("{:#?}", hero);
-    hero.strike();
-    println!("{:#?}", hero);
-    println!("{:#?}", goblin);
-    goblin.strike();
-    println!("{:#?}", goblin);
+    let thread_1 = thread::spawn(|| "Hello");
+    thread::sleep(std::time::Duration::from_millis((100)));
+    let thread_2 = thread::spawn(|| "Rust");
+    println!("{:?}", thread_2.join().unwrap());
+    println!("{:?}", thread_1.join().unwrap());
 }
 
 //
