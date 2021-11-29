f21548352e1ac717c9d94bfa838e98a0714f0560
Perry Anderson <perry.anderson@gmail.com>
2021-11-29 11:46:28 -0800

    Added MineField

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,55 @@
 //
+// https://www.how2shout.com/linux/how-to-install-brew-ubuntu-20-04-lts-linux/
 // https://levelup.gitconnected.com/rust-with-visual-studio-code-46404befed8
 // https://github.com/reem/stainless
 //
 
-use std::thread;
+mod structs;
+extern crate rand;
+use rand::Rng;
+use std::io;
+use structs::MineField;
 
 fn main() {
-    let thread_1 = thread::spawn(|| "Hello");
-    thread::sleep(std::time::Duration::from_millis((100)));
-    let thread_2 = thread::spawn(|| "Rust");
-    println!("{:?}", thread_2.join().unwrap());
-    println!("{:?}", thread_1.join().unwrap());
+    let mut mine_field = MineField {
+        size: (3, 3),
+        mines: Vec::new(),
+    };
+    let _ = &mine_field.generate_mines();
+    let _ = &mine_field.print_fields();
+    let mut x = 0;
+    let mut y = 0;
+    println!("You have 5 tries");
+    let mut counter = 1;
+    loop {
+        if counter == 5 {
+            println!("Congrats")
+        }
+        println!("Enter coordinate x:");
+        let mut input_x = String::new();
+        let _ = io::stdin().read_line(&mut input_x);
+        let trimmed_x = input_x.trim();
+        match trimmed_x.parse::<u32>() {
+            Ok(i) => x = i,
+            Err(..) => println!("This was not an integer: {}", trimmed_x),
+        }
+        println!("Enter coordinate y:");
+        let mut input_y = String::new();
+        let _ = io::stdin().read_line(&mut input_y);
+        let trimmed_y = input_y.trim();
+        match trimmed_y.parse::<u32>() {
+            Ok(i) => y = i,
+            Err(..) => println!("This was not an integer: {}", trimmed_y),
+        }
+
+        if mine_field.find_by_coordinates(x, y) {
+            println!("The game is over :(");
+            break;
+        }
+        counter = counter + 1;
+    }
+    println!("The mines are marked with o: ");
+    mine_field.print_fields_solved();
 }
 
 //
